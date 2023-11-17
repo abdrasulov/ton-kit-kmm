7a0326bb005d0a02c64c63f474651dd9a873439a
abdrasulov <abdrasulov@gmail.com>
2023-11-17 11:39:40 +0600

    Add methods to observe updates

--- shared/src/commonMain/kotlin/io/horizontalsystems/tonkit/TonKit.kt
+++ shared/src/commonMain/kotlin/io/horizontalsystems/tonkit/TonKit.kt
@@ -1,5 +1,10 @@
 package io.horizontalsystems.tonkit
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
 class TonKit(
     private val transactionManager: TransactionManager,
     private val balanceManager: BalanceManager,
@@ -13,11 +18,14 @@ class TonKit(
     val balanceSyncStateFlow by syncer::balanceSyncStateFlow
     val transactionsSyncStateFlow by syncer::transactionsSyncStateFlow
 
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+
     fun start() {
         syncer.start()
     }
 
     fun stop() {
+        coroutineScope.cancel()
         syncer.stop()
     }
 
@@ -31,5 +39,29 @@ class TonKit(
 
     suspend fun transactions(fromTransactionHash: String?, type: TransactionType?, limit: Long): List<TonTransaction> {
         return transactionManager.transactions(fromTransactionHash, type, limit)
+    }
+
+    fun onNewTransactions(callback: (List<TonTransaction>) -> Unit) {
+        coroutineScope.launch {
+            newTransactionsFlow.collect(callback)
+        }
+    }
+
+    fun onBalance(callback: (String?) -> Unit) {
+        coroutineScope.launch {
+            balanceFlow.collect(callback)
+        }
+    }
+
+    fun onBalanceSyncState(callback: (SyncState) -> Unit) {
+        coroutineScope.launch {
+            balanceSyncStateFlow.collect(callback)
+        }
+    }
+
+    fun onTransactionsSyncState(callback: (SyncState) -> Unit) {
+        coroutineScope.launch {
+            transactionsSyncStateFlow.collect(callback)
+        }
     }
 }
